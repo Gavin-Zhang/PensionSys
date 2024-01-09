efec4925ea3ea1df267593d746090465fb493f54
Gavin <yinyuqni1@163.com>
2024-01-09 08:01:23 +0800

    添加离院处理

--- admin/jly/js/base.js
+++ admin/jly/js/base.js
@@ -5,6 +5,22 @@
 // ;var CookieName = "bm-cookie";
 ;var key_enter = 13;
 
+Date.prototype.Format = function(fmt) {
+	var o = {
+		"M+": this.getMonth() + 1,
+		"d+": this.getDate(),
+		"H+": this.getHours(),
+		"m+": this.getMinutes(),
+		"s+": this.getSeconds(),
+		"q+": Math.floor((this.getMonth()+3)/3),
+		"S": this.getMilliseconds()
+	};
+	
+	if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4-RegExp.$1.length))
+	for (var k in o)
+		if (new RegExp("("+k+")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1)?(o[k]):(("00"+o[k]).substr((""+o[k]).length)))
+	return fmt;
+}
 function GetCookie(name) {
 	if (document.cookie.length > 0) {
 		var start = document.cookie.indexOf(name + '=');
